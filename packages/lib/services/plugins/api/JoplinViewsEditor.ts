e901ab67f68344f13015e7ecc0bf09a3225c533c
Laurent Cozic <laurent22@users.noreply.github.com>
2024-10-30 23:57:18 +0000

    doc

--- packages/lib/services/plugins/api/JoplinViewsEditor.ts
+++ packages/lib/services/plugins/api/JoplinViewsEditor.ts
@@ -5,6 +5,20 @@ import createViewHandle from '../utils/createViewHandle';
 import WebviewController, { ContainerType } from '../WebviewController';
 import { ViewHandle } from './types';
 
+/**
+ * Allows creating alternative note editors. When `show` is called, this view is going to replace
+ * the note editor - you can then handle loading and saving note, and do your own rendering.
+ *
+ * Although it may be used to implement an alternative text editor, the more common use case may be
+ * to render the note in a different, graphical way - for example displaying a graph, and
+ * saving/loading the graph data in the associated note. In that case, you would detect whether the
+ * current note contains graph data and, in this case, you'd display your viewer. If not, you would
+ * hide it.
+ *
+ * Usually you would listen to the `onNoteChange` event - at that point check the note content, and
+ * if your plugin can render it, then call `show()`. Otherwise make sure to call `hide()`, so that
+ * the view is not unnecessarily being displayed.
+ */
 export default class JoplinViewsEditors {
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
@@ -46,14 +60,25 @@ export default class JoplinViewsEditors {
 		return this.controller(handle).postMessage(message);
 	}
 
+	/**
+	 * Tells the application that the editor may be shown. Note that the user can toggle between
+	 * your editor and the built-in text editor, so you are not guaranteed that the editor will
+	 * actually be visible. This only tells the app that the user can choose to display the editor.
+	 */
 	public async show(handle: ViewHandle, show = true): Promise<void> {
 		await this.controller(handle).show(show);
 	}
 
+	/**
+	 * Tells the application that the editor is not available in the current context.
+	 */
 	public async hide(handle: ViewHandle): Promise<void> {
 		await this.show(handle, false);
 	}
 
+	/**
+	 * Tells whether the editor is visible or not.
+	 */
 	public async visible(handle: ViewHandle): Promise<boolean> {
 		return this.controller(handle).visible;
 	}
