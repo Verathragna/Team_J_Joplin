acb58154f879feda6d3d124701827231a728b303
pedr <pedrlz.frn@gmail.com>
2024-12-17 11:36:11 -0300

    adding disabled keys accordion

--- packages/app-mobile/components/screens/encryption-config.tsx
+++ packages/app-mobile/components/screens/encryption-config.tsx
@@ -9,9 +9,10 @@ import time from '@joplin/lib/time';
 import { decryptedStatText, enableEncryptionConfirmationMessages, onSavePasswordClick, useInputMasterPassword, useInputPasswords, usePasswordChecker, useStats } from '@joplin/lib/components/EncryptionConfigScreen/utils';
 import { MasterKeyEntity } from '@joplin/lib/services/e2ee/types';
 import { State } from '@joplin/lib/reducer';
-import { SyncInfo } from '@joplin/lib/services/synchronizer/syncInfoUtils';
+import { masterKeyEnabled, SyncInfo } from '@joplin/lib/services/synchronizer/syncInfoUtils';
 import { getDefaultMasterKey, setupAndDisableEncryption, toggleAndSetupEncryption } from '@joplin/lib/services/e2ee/utils';
 import { useMemo, useState } from 'react';
+import { Divider, List } from 'react-native-paper';
 import shim from '@joplin/lib/shim';
 
 interface Props {
@@ -34,8 +35,10 @@ const EncryptionConfigScreen = (props: Props) => {
 	const { passwordChecks, masterPasswordKeys } = usePasswordChecker(props.masterKeys, props.activeMasterKeyId, props.masterPassword, props.passwords);
 	const { inputPasswords, onInputPasswordChange } = useInputPasswords(props.passwords);
 	const { inputMasterPassword, onMasterPasswordSave, onMasterPasswordChange } = useInputMasterPassword(props.masterKeys, props.activeMasterKeyId);
+	const [showDisabledKeys, setShowDisabledKeys] = useState(false);
 
 	const mkComps = [];
+	const disabledMkComps = [];
 
 	const nonExistingMasterKeyIds = props.notLoadedMasterKeys.slice();
 
@@ -77,6 +80,10 @@ const EncryptionConfigScreen = (props: Props) => {
 			container: {
 				flex: 1,
 				padding: theme.margin,
+			},
+			disabledContainer: {
+				paddingLeft: theme.margin,
+				paddingRight: theme.margin,
 			},
 		};
 
@@ -226,14 +233,17 @@ const EncryptionConfigScreen = (props: Props) => {
 		}
 	};
 
-
-
-	for (let i = 0; i < props.masterKeys.length; i++) {
+	for (let i = 0; i < props.masterKeys.filter(mk => masterKeyEnabled(mk)).length; i++) {
 		const mk = props.masterKeys[i];
-		mkComps.push(renderMasterKey(i + 1, mk));
+		mkComps.push(renderMasterKey(mk));
 
 		const idx = nonExistingMasterKeyIds.indexOf(mk.id);
 		if (idx >= 0) nonExistingMasterKeyIds.splice(idx, 1);
+	}
+
+	for (let i = 0; i < props.masterKeys.filter(mk => !masterKeyEnabled(mk)).length; i++) {
+		const mk = props.masterKeys[i];
+		disabledMkComps.push(renderMasterKey(mk));
 	}
 
 	const onToggleButtonClick = async () => {
@@ -286,27 +296,39 @@ const EncryptionConfigScreen = (props: Props) => {
 	return (
 		<View style={rootStyle}>
 			<ScreenHeader title={_('Encryption Config')} />
-			<ScrollView style={styles.container}>
-				<View style={{ backgroundColor: theme.warningBackgroundColor, paddingTop: 5, paddingBottom: 5, paddingLeft: 10, paddingRight: 10 }}>
-					<Text>{_('For more information about End-To-End Encryption (E2EE) and advice on how to enable it please check the documentation:')}</Text>
-					<TouchableOpacity
-						onPress={() => {
-							Linking.openURL('https://joplinapp.org/help/apps/sync/e2ee');
-						}}
-					>
-						<Text>https://joplinapp.org/help/apps/sync/e2ee</Text>
-					</TouchableOpacity>
-				</View>
+			<ScrollView>
+				<View style={styles.container}>
+					<View style={{ backgroundColor: theme.warningBackgroundColor, paddingTop: 5, paddingBottom: 5, paddingLeft: 10, paddingRight: 10 }}>
+						<Text>{_('For more information about End-To-End Encryption (E2EE) and advice on how to enable it please check the documentation:')}</Text>
+						<TouchableOpacity
+							onPress={() => {
+								Linking.openURL('https://joplinapp.org/help/apps/sync/e2ee');
+							}}
+						>
+							<Text>https://joplinapp.org/help/apps/sync/e2ee</Text>
+						</TouchableOpacity>
+					</View>
 
-				<Text style={styles.titleText}>{_('Status')}</Text>
-				<Text style={styles.normalText}>{_('Encryption is: %s', props.encryptionEnabled ? _('Enabled') : _('Disabled'))}</Text>
-				{decryptedItemsInfo}
-				{renderMasterPassword()}
-				{toggleButton}
-				{passwordPromptComp}
-				{mkComps}
-				{nonExistingMasterKeySection}
-				<View style={{ flex: 1, height: 20 }}></View>
+					<Text style={styles.titleText}>{_('Status')}</Text>
+					<Text style={styles.normalText}>{_('Encryption is: %s', props.encryptionEnabled ? _('Enabled') : _('Disabled'))}</Text>
+					{decryptedItemsInfo}
+					{renderMasterPassword()}
+					{toggleButton}
+					{passwordPromptComp}
+					{mkComps}
+					{nonExistingMasterKeySection}
+				</View>
+				<Divider />
+				<List.Accordion
+					title={_('Disabled keys')}
+					titleStyle={styles.titleText}
+					expanded={showDisabledKeys}
+					onPress={() => setShowDisabledKeys(st => !st)}
+				>
+					<View style={styles.disabledContainer}>
+						{disabledMkComps}
+					</View>
+				</List.Accordion>
 			</ScrollView>
 		</View>
 	);
