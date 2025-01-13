9794491cffdb2856eb78dcc2672d08103feef845
pedr <pedrlz.frn@gmail.com>
2025-01-13 17:39:28 -0300

    renaming in favour of current pattern

--- packages/app-desktop/gui/SearchBar/SearchBar.tsx
+++ packages/app-desktop/gui/SearchBar/SearchBar.tsx
@@ -174,7 +174,7 @@ function SearchBar(props: Props) {
 				onKeyDown={onKeyDown}
 				onSearchButtonClick={onSearchButtonClick}
 				searchStarted={searchStarted}
-				ariaControls='search-results'
+				aria-controls='search-results'
 			/>
 		</Root>
 	);
